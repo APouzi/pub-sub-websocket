1a6813bd93e791e28d3a3c276da72e110c58ac15
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-09-13 00:45:31 +0100

    adding imports and golang http loop and handlefunc -AP

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
 func main() {
 	println("Golang Server Started")
+	http.HandleFunc("/ws-upgrade",wsUpgrade)
+	http.ListenAndServe(":8080",nil)
+}
 }
