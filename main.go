c2747d9a30c9ad0ff848b2fcbb7ffa8f2cd4db9f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-09-13 00:48:26 +0100

    starting the gobwas highlevel websocket -AP

--- main.go
+++ main.go
@@ -13,4 +13,22 @@ func main() {
 	http.HandleFunc("/ws-upgrade",wsUpgrade)
 	http.ListenAndServe(":8080",nil)
 }
+
+func wsUpgrade(w http.ResponseWriter, r *http.Request){
+	print("Upgrading to WebSockets")
+	conn, wr, _,err := ws.UpgradeHTTP(r,w)
+	if err != nil{
+		println("connection failed")
+	}
+	print(conn)
+	for {
+		payload, err := wsutil.ReadClientText(wr)
+		if err != nil{
+			print("err reading client text")
+		}
+		if strings.Contains(string(payload),"sub"){
+			println("has sub")
+		}
+	}
+
 }
