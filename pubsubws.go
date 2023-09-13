f9121458ab765c67d2137f39e14506e1333b6731
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-09-13 01:56:35 +0100

    pubsubws.go setup for foundation on websocket pubsub -AP

---
+++ pubsubws.go
@@ -0,0 +1,19 @@
+package main
+
+import "bufio"
+
+type PubSub struct {
+
+}
+
+func InitiatePubSubFeed() *PubSub{
+ 
+}
+
+func(pb *PubSub) Subscribe(topic string, wr *bufio.ReadWriter) {
+	
+}
+
+func(pb *PubSub) Publish(topic string,message string){
+
+}
