c4241b1fbef9d1dddb0e609cfeb37a36f9925a9e
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-09-13 02:13:20 +0100

    Publish in pubsub start -AP

--- pubsubws.go
+++ pubsubws.go
@@ -3,17 +3,27 @@ package main
 import "bufio"
 
 type PubSub struct {
-
+	Subscribers map[string][]*bufio.ReadWriter
 }
 
 func InitiatePubSubFeed() *PubSub{
- 
+	init := make(map[string][] *bufio.ReadWriter)
+	return &PubSub{Subscribers: init} 
 }
 
 func(pb *PubSub) Subscribe(topic string, wr *bufio.ReadWriter) {
-	
+	pb.Subscribers[topic] = append(pb.Subscribers[topic], wr)
 }
 
 func(pb *PubSub) Publish(topic string,message string){
+	if len(pb.Subscribers) == 0{
+		return
+	}
+	
+}
+
+// Need to figure out how I should implement the closing of a connection. 
+// If connection is closed and it's inside of a list, what happens to that closed connection
+func(pb *PubSub) UnSub(){
 
 }
